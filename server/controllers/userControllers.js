e3e658c96f735e2c6ad7f095cf71e6c217297da7
Dravingupta <dravingupta12@gmail.com>
2025-12-21 16:37:53 +0530

    auth code Changes

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,19 +2,31 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 
-async function UserSignup(req , res) {
+async function UserSignup(req, res) {
     try {
-    
-            const { username, email , password } = req.body;
-            const hashedpass = bcrypt.hash(password, 10);
-            const user = new User({ username, email, password : hashedpass });
-            await user.save();
-            res.status(200).json(user)
-        }catch(err){
-        console.log(err);   
-   res.status(500).json({ message: err });
+
+        const { username, email, password } = req.body;
+        const hashedpass = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedpass });
+        await user.save();
+        res.status(200).json({
+            massage: "user created", user
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
     }
 
 };
+
+async function UserLogin(req, res) {
+    try {
+        const { email, password } = req.body;
+
+    } catch (error) {
+
+    }
+
+}
 
 
