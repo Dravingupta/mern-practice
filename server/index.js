1963068c541b4afd31f6b6b80bd454efeb8ad13c
Dravin gupta <dravingupta12@gmail.com>
2026-02-14 14:09:31 +0530

    hhhh

--- server/index.js
+++ server/index.js
@@ -10,15 +10,15 @@ const User = require("./models/User")
 
 
 connectDB()
-.then(() => {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch(err => {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1);
     });
-})
-.catch(err => {
-    console.error('Failed to connect to the database:', err);
-    process.exit(1);
-});
 
 app.use(cors({
     origin: "http://localhost:5173", // React app
@@ -28,15 +28,15 @@ app.use(cookieParser());
 app.use(express.json());
 
 
-app.use('/tasks' , taskRoutes);  
-app.use('/auth' , UserRoutes); 
-app.get("/me", auth , (req , res)=>{
+app.use('/tasks', taskRoutes);
+app.use('/auth', UserRoutes);
+app.get("/me", auth, (req, res) => {
 
     res.json({
         "message": "yes",
-        "id":req.userid
+        "id": req.userid
     })
-} ) 
+})
 
 
 
